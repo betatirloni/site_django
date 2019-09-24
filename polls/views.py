5d8df0bc881ed8631bee650304777659330a9882
Roberta Tirloni <betatirloni@gmail.com>
2019-09-23 22:06:25 -0300

    teste

--- polls/views.py
+++ polls/views.py
@@ -14,7 +14,7 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
     
     def get_queryset(self):
-        """Return the last five published questions."""
+        """Return the last five published dvdsv questions."""
         return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
